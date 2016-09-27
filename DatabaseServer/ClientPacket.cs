3294656706156ee7e8036dd2f0b43617e680cf2e
victor <waterwolf@ukr.net>
2016-09-27 23:56:58 +0400

    Create client packet

---
+++ DatabaseServer/ClientPacket.cs
@@ -0,0 +1,18 @@
+﻿
+namespace DatabaseServer
+{
+    class ClientPacket
+    {
+        public string Query { get; set; }
+
+        public ClientPacket(string query)
+        {
+            Query = query;
+        }
+
+        public override string ToString()
+        {
+            return Query;
+        }
+    }
+}
